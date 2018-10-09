15c25da74d2de41e2a5d36e5e0d3fc98937b5fa6
Natalie Stimpson <n-stimpson@hotmail.com>
2018-10-09 10:39:51 +0100

    new xcode project created
    new xcode project created

---
+++ 'Bout time/'Bout time/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  'Bout time
+//
+//  Created by James Devlin on 09/10/2018.
+//  Copyright © 2018 Natalie Stimpson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
