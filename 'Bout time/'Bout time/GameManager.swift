ae960783eebeb8b03b8ea73f742834fbe87ccd2b
Natalie Stimpson <n-stimpson@hotmail.com>
2018-10-11 13:04:00 +0100

    added scoring and multiple rounds
    added scoring and multiple rounds

--- 'Bout time/'Bout time/GameManager.swift
+++ 'Bout time/'Bout time/GameManager.swift
@@ -11,8 +11,10 @@ struct EventsForRound {
 
 class GameManager {
     var roundNumber: Int = 0
+    var runningScore: Int = 0
     let eventsPerRound = 4
     let totalNumberOfRounds = 6
+    let secondsForTimer = 10
     let allEvents = AllEvents()
     
     func shuffleEvents() -> [Event] {
@@ -22,17 +24,46 @@ class GameManager {
     }
     
     func pickEventsForRound(shuffledEvents: [Event]) -> EventsForRound {
-        let eventsForRound: EventsForRound = EventsForRound(eventOne: shuffledEvents[0 + roundNumber], eventTwo: shuffledEvents[1 + roundNumber], eventThree: shuffledEvents[2 + roundNumber], eventFour: shuffledEvents[3 + roundNumber])
+        let eventsForRound: EventsForRound = EventsForRound(eventOne: shuffledEvents[0], eventTwo: shuffledEvents[1], eventThree: shuffledEvents[2], eventFour: shuffledEvents[3])
         return eventsForRound
     }
     
-    /*func checkOrder(ofEventsUsedInRound events: EventsForRound) -> Bool {
-        if events.eventOne.date < events.eventTwo.date && events.eventTwo.date < events.eventThree.date && events.eventThree.date < events.eventFour.date {return true}
-        else {return false}
+    
+    enum dateError: Error {
+        case invalidData
     }
     
-    */
+    
+    func checkOrderOfTwoDates(isDate dateA: DateComponents, before dateB: DateComponents) throws -> Bool {
+    
+        
+        guard let yearA = dateA.year, let yearB = dateB.year, let monthA = dateA.month, let monthB = dateB.month, let dayA = dateA.day, let dayB = dateB.day else {throw dateError.invalidData}
+        
+        let answer: Bool
+        
+        if yearA < yearB {answer = true}
+        else if yearA == yearB && monthA < monthB {answer = true}
+        else if yearA == yearB && monthA == monthB && dayA <= dayB {answer = true}
+        else{answer = false}
+        return answer
+}
+        
+    func checkAnswer(ofEventsUsedInRound events: EventsForRound) -> Bool {
+        
+            let oneBeforeTwo: Bool
+            let twoBeforeThree: Bool
+            let threeBeforeFour: Bool
+        
+            do{ oneBeforeTwo = try checkOrderOfTwoDates(isDate:         events.eventOne.date, before: events.eventTwo.date)
+                twoBeforeThree = try checkOrderOfTwoDates(isDate: events.eventTwo.date, before: events.eventThree.date)
+                threeBeforeFour = try checkOrderOfTwoDates(isDate: events.eventThree.date, before: events.eventFour.date)
+                return oneBeforeTwo && twoBeforeThree && threeBeforeFour
+    }
+           //fix error handling here
+            catch {return false}
+
         
 }
 
 
+}
--- 'Bout time/'Bout time/ViewController.swift
+++ 'Bout time/'Bout time/ViewController.swift
@@ -1,12 +1,12 @@
 //
-//  ViewController.swift
-//  'Bout time
-//
-//  Created by James Devlin on 09/10/2018.
-//  Copyright © 2018 Natalie Stimpson. All rights reserved.
-//
+//  CHANGE TIMER TO 60 SECONDS
+//fix timer
+// fix error throwing
+//add safari view controller
+
 
 import UIKit
+let gameManager = GameManager()
 
 class ViewController: UIViewController {
 
@@ -24,6 +24,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var thirdButtonDown: UIButton!
     @IBOutlet weak var fourthButton: UIButton!
     
+    @IBOutlet weak var nextRoundFailure: UIButton!
+    @IBOutlet weak var nextRoundSuccess: UIButton!
+    @IBOutlet weak var shakeToCompleteLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var linkToFurtherInfo: UIButton!
+    @IBOutlet weak var finalScore: UIButton!
+    @IBOutlet weak var score: UILabel!
+    @IBOutlet weak var playAgain: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,17 +55,131 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    var eventsForRound = GameManager().pickEventsForRound(shuffledEvents: GameManager().shuffleEvents())
+    var eventsForRound = gameManager.pickEventsForRound(shuffledEvents: gameManager.shuffleEvents())
     
     func displayEvents() {
-        questionText.text = "In what order were these 90's number one hits released?"
+        
+        questionText.text = "In what order did these 90's hits reach number one?"
+        
+        timerLabel.isHidden = false
+        shakeToCompleteLabel.isHidden = false
+        
         firstEvent.text = eventsForRound.eventOne.description
         secondEvent.text = eventsForRound.eventTwo.description
         thirdEvent.text = eventsForRound.eventThree.description
         fourthEvent.text = eventsForRound.eventFour.description
         
+        timerLabel.text = "X:X"
+        
+        nextRoundSuccess.isHidden = true
+        nextRoundFailure.isHidden = true
+        linkToFurtherInfo.isHidden = true
+        finalScore.isHidden = true
+        score.isHidden = true
+        playAgain.isHidden = true
+        
+       // timer(seconds: gameManager.secondsForTimer)
         
     }
+    
+    
+    func checkAnswer() {
+        shakeToCompleteLabel.isHidden = true
+        timerLabel.isHidden = true
+        linkToFurtherInfo.isHidden = false
+        
+        let answer = GameManager().checkAnswer(ofEventsUsedInRound: eventsForRound)
+        
+        if answer {
+            nextRoundSuccess.isHidden = false
+            gameManager.runningScore += 1
+        }
+        else {
+            nextRoundFailure.isHidden = false
+        }
+        
+        gameManager.roundNumber += 1
+       
+    }
+    
+    func timer(seconds: Int) {
+        // Converts a delay in seconds to nanoseconds as signed 64 bit integer
+        let delay = Int64(NSEC_PER_SEC * UInt64(seconds))
+        // Calculates a time value to execute the method given current time and delay
+        let dispatchTime = DispatchTime.now() + Double(delay) / Double(NSEC_PER_SEC)
+        // Executes the nextRound method at the dispatch time on the main queue
+        DispatchQueue.main.asyncAfter(deadline: dispatchTime) {
+            self.checkAnswer()
+        }
+    }
+    
+    func nextRound() {
+        if gameManager.roundNumber == gameManager.totalNumberOfRounds {
+            // Game is over
+            gameOver()
+        } else {
+            // Continue game
+            eventsForRound = gameManager.pickEventsForRound(shuffledEvents: gameManager.shuffleEvents())
+            displayEvents()
+            
+        }
+    }
+    
+    
+    func gameOver() {
+        
+        linkToFurtherInfo.isHidden = true
+        nextRoundFailure.isHidden = true
+        nextRoundSuccess.isHidden = true
+        firstEvent.isHidden = true
+        secondEvent.isHidden = true
+        thirdEvent.isHidden = true
+        fourthEvent.isHidden = true
+        firstButton.isHidden = true
+        secondButton.isHidden = true
+        secondButtonDown.isHidden = true
+        thirdButton.isHidden = true
+        thirdButtonDown.isHidden = true
+        fourthButton.isHidden = true
+        finalScore.isHidden = false
+        
+        questionText.text = "GAME OVER!"
+        
+    }
+    
+    func showScore() {
+    
+    finalScore.isHidden = true
+    score.isHidden  = false
+    playAgain.isHidden = false
+        
+        questionText.text = "Way to go! You scored:"
+        score.text = "\(gameManager.runningScore)/\(gameManager.totalNumberOfRounds)"
+
+    }
+    
+    func newGame() {
+        gameManager.runningScore = 0
+        gameManager.roundNumber = 0
+        
+        score.isHidden  = true
+        playAgain.isHidden = true
+        firstEvent.isHidden = false
+        secondEvent.isHidden = false
+        thirdEvent.isHidden = false
+        fourthEvent.isHidden = false
+        firstButton.isHidden = false
+        secondButton.isHidden = false
+        secondButtonDown.isHidden = false
+        thirdButton.isHidden = false
+        thirdButtonDown.isHidden = false
+        fourthButton.isHidden = false
+        timerLabel.isHidden = false
+        
+        displayEvents()
+        
+    }
+    
     
     @IBAction func switchOneTwo(_ sender: UIButton) {
     
@@ -101,6 +223,26 @@ class ViewController: UIViewController {
         fourthEvent.text = eventsForRound.eventFour.description
     }
     
+    
+    
+    @IBAction func nextRound(_ sender: UIButton) {
+    nextRound()
+    }
+    
+    
+    @IBAction func revealScore(_ sender: UIButton) {
+        showScore()
+    }
+    
+    
+    
+    @IBAction func playAgain(_ sender: UIButton) {
+        newGame()
+    }
+    
+    
+    
+
     override var canBecomeFirstResponder: Bool{
         get {
         return true
@@ -108,14 +250,15 @@ class ViewController: UIViewController {
         
     }
     
+
     override func motionEnded(
         _ motion: UIEvent.EventSubtype, with Event: UIEvent?)
     {
         if motion == .motionShake {
-             thirdEvent.text = "why are you shaking me???"
+          self.checkAnswer()
         }
+    
+    
     }
-    
-    
-}
 
+}
